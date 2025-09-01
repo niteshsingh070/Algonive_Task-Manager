b49c82295551990247ddb71ad00cc0469229ca7c
Nitesh Singh <146375161+niteshsingh070@users.noreply.github.com>
2025-09-01 13:56:28 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: process.env.CORS_ORIGIN ||'http://localhost:5173'
 }))
 
 //routes
